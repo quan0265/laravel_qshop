3a45f0eb584ee04057ea00dbac3c5f1b86f280f2
quan0265 <quan0265@gmail.com>
2021-03-08 21:49:36 +0700

    add SlidersSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,31 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('sliders')->insert([
+            [
+            	'name' => 'slider1',
+	            'image' => '04-03-46-05-03-2021.jpg',
+	            'status' => 1,
+            ],
+            [
+            	'name' => 'slider2',
+	            'image' => '04-03-26-05-03-2021.jpg',
+	            'status' => 1,
+            ],
+            [
+            	'name' => 'slider3',
+	            'image' => '04-03-44-05-03-2021.jpg',
+	            'status' => 1,
+            ],
+            [
+            	'name' => 'slider4',
+	            'image' => '04-03-06-05-03-2021.jpg',
+	            'status' => 1,
+            ],
+        ]);
+
+
+
+        
     }
 }
